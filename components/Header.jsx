e21a75b5a574a5c20db429b5e349b375150e487c
Akhil Jinu <portforwarding03@gmail.com>
2022-10-16 00:14:01 +0530

    1

---
+++ components/Header.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+
+
+const Header = () => {
+
+
+
+    return (
+
+        <>
+
+            <div className='sticky top-[0%]'>
+                <nav class="bg-white   w-full shadow  ">
+                    <div class="mx-auto px-8">
+                        <div className='flex items-center justify-between h-16'>
+
+                            <div>
+                                <h1 className='font-medium text-lg antialiased text-black'>
+                                    Total Scan
+                                </h1>
+                            </div>
+
+                            <div className='cursor-pointer border-2 py-1 px-3'>
+                                <h5 className=" font-semibold text-black text-sm " >Login</h5>
+
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
+        </>
+    )
+}
+
+export default Header
---
+++ components/Search.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+function Search() {
+    return (
+        <>
+            <form onSubmit={""} className='absolute z-[9999] top-[45%] w-[40%] left-[29%] '>
+                <div className='relative'>
+
+
+                    {/* <span class="block text-sm font-extrabold  text-transparent bg-clip-text bg-gradient-to-r  from-purple-600 to-pink-700 ">Anime Title</span> */}
+                    <input type="text" required onChange={""} placeholder='URL, IP address, domain' className='mt-2 block w-full px-3 py-2 bg-white border border-slate-500  text-sm text-black  placeholder-slate-500 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'></input>
+
+                    {/* <div className="mt-2 ">
+                        <button
+                            type="submit"
+                            className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white  rounded-md border border-slate-300 hover:text-zinc-300 "
+
+                        >
+                            Search
+                        </button>
+
+
+                    </div> */}
+
+                   
+                </div>
+            </form>
+
+           
+        </>
+    )
+    
+}
+
+export default Search
