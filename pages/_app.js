dff50c3468f5847d1233aed1ded1907d807a6221
Akhil Jinu <portforwarding03@gmail.com>
2022-10-16 13:35:08 +0530

    login and signup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import createEmotionCache from '../util/cache';
+import lightTheme from '../styles/theme';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const clientSideEmotionCache = createEmotionCache();
+
+
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
+
 }
 
 export default MyApp
