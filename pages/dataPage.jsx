d181615e792403faf4972d035b6ed06f9178b321
1uciferx <portforwarding03@gmail.com>
2022-12-28 15:06:17 +0530

    shodan upgrade

--- pages/dataPage.jsx
+++ pages/dataPage.jsx
@@ -34,7 +34,10 @@ function dataPage() {
 
   const [apiData, setData] = useState()
 
-  const [tableData, setTable] = useState([])
+
+  const [shodan, setShodan] = useState([]);
+
+  const [tableData, setTable] = useState([]);
 
   const [value, setValue] = useState('1');
 
@@ -56,11 +59,13 @@ function dataPage() {
   };
 
 
+
+
   useEffect(() => {
     axios
       .request(options)
       .then(function (response) {
-        console.log("aspin", response.data);
+        console.log("royal", response.data);
         setData(response.data)
       })
       .catch(function (error) {
@@ -73,17 +78,36 @@ function dataPage() {
 
   useEffect(() => {
     setTable(apiData && apiData.data.attributes.last_analysis_results)
-
+    shodanData()
   }, [apiData])
 
   const result = tableData && Object.entries(tableData);
 
   console.log("-->", apiData && apiData.data.id);
 
-  result && result.forEach(([key, value]) => {
-    console.log(key); // 'one'
-    console.log(value); // 1
-  });
+
+  // const s = shodan && Object.entries(shodan);
+
+  // s && s.forEach(([key, value]) => {
+  //   console.log(key); // 'one'
+  //   console.log(value); // 1
+  // });
+
+
+
+
+
+  const shodanData = async () => {
+    if (apiData && apiData.data.id) {
+      const { data } = await axios.get(`https://api.shodan.io/shodan/host/${apiData && apiData.data.id}?key=MuWfcU97yw8u9XP08ZsROsYTiny7Ibcx`)
+
+      setShodan(data)
+
+    }
+
+  }
+
+  console.log("make-->", shodan)
 
 
   return (
@@ -175,7 +199,7 @@ function dataPage() {
                 <TabList onChange={handleChange} aria-label="lab API tabs example">
                   <Tab label="DETECTION" value="1" />
                   <Tab label="DETAILS" value="2" />
-                  <Tab label="RELATIONS" value="3" />
+                  {/* <Tab label="RELATIONS" value="3" /> */}
                 </TabList>
               </Box>
 
@@ -223,7 +247,7 @@ function dataPage() {
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
                                   </svg>
 
-                                  <h1  className="ml-[10px]">{value.result}</h1>
+                                  <h1 className="ml-[10px]">{value.result}</h1>
                                 </div>
 
                               </>
@@ -238,8 +262,43 @@ function dataPage() {
                   </Table>
                 </TableContainer>
               </TabPanel>
-              <TabPanel value="2">Item Two</TabPanel>
-              <TabPanel value="3">Item Three</TabPanel>
+              <TabPanel value="2">
+                From Shodan
+
+                <TableContainer component={Paper}>
+                  <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                    <TableHead>
+                      <TableRow>
+                      <TableCell>Country_Code</TableCell>
+                        <TableCell align="right">region_code</TableCell>
+                        <TableCell align="right">City</TableCell>
+                        <TableCell align="right">Country Name</TableCell>
+                        <TableCell align="right">ISP</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                     
+                        <TableRow
+                          // key={row.name}
+                          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                          <TableCell component="th" scope="row">
+                            {shodan && shodan.country_code}
+                          </TableCell>
+                          <TableCell align="right">{shodan && shodan.region_code}</TableCell>
+                          <TableCell align="right">{shodan && shodan.city}</TableCell>
+                          <TableCell align="right">{shodan && shodan.country_name}</TableCell>
+                          <TableCell align="right">{shodan && shodan.isp}</TableCell>
+                        </TableRow>
+                     
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+
+
+                
+              </TabPanel>
+              {/* <TabPanel value="3">Item Three</TabPanel> */}
             </TabContext>
           </Box>
         </Container>
